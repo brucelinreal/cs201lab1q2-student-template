595102cf46a1c24704cd4e2dee8a61c3720ceb86
brucelin <lukjolin15@gmail.com>
2026-09-04 00:30:13 +0800

    hiks

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -77,6 +77,7 @@ public class SinglyLinkedList<E> {
     }
 
     public E removeLast(){
+
         Node<E> current = head; 
         E res; 
 
@@ -88,6 +89,7 @@ public class SinglyLinkedList<E> {
             res = head.getElement(); 
             head = null;
             tail = null; 
+            size--;
             return res; 
         }
 
